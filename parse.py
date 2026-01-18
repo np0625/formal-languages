fd644bd2f1f6ac00b8993e1958939228d1c2d433
Nishad Prakash <nishad@gmail.com>
2026-01-18 14:30:51 -0500

    Literals and unions of literals work

--- parse.py
+++ parse.py
@@ -74,10 +74,10 @@ In short, the graph offers speculative, computation-driven connections that do n
 # "re string" -> RE IR -> NFA
 
 
-def matcher_aux(s, re, s_idx, re_idx, s_len, re_len):
+def matcher_aux_old(s, re, s_idx, re_idx, s_len, re_len, match_report):
     done = False
     while not done:
-        if re_idx == re_len: # later add s_idx == s_len
+        if re_idx >= re_len: # later add s_idx >= s_len
             break
         # cur_char = s[s_idx]
         cur_regex = re[re_idx]
@@ -86,40 +86,168 @@ def matcher_aux(s, re, s_idx, re_idx, s_len, re_len):
         match cur_regex_type:
             case 'literal':
                 print(f"a literal: {cur_regex}")
-                s_idx += 1
-                re_idx += 1
+                cur_char = s[s_idx]
+                rep = {'expr': cur_regex, 'start_pos': s_idx, 'end_pos': s_idx}
+                if cur_char == cur_regex_expr:
+                    s_idx += 1
+                    re_idx += 1
+                    rep['success'] = True
+                    match_report.append(rep)
+                else:
+                    rep['success'] = False
+                    match_report.append(rep)
+                    break
             case 'union':
                 print(f"the union for ever: {cur_regex_expr}")
+                saved_s_idx = s_idx
+                reports = []
+                found_match = False
                 for r in cur_regex_expr:
-                    print(r)
-                    # updates s_idx globally, uses local re_idx
-                    matcher_aux(s, (r, ), s_idx, 0, s_len, 1)
-                re_idx += 1
-            case 'star':
-                print(f"the big star: {cur_regex_expr}")                
-                matcher_aux(s, cur_regex_expr, s_idx, 0, s_len, len(cur_regex_expr))
+                    print(r)                    
+                    # Wrap each sub-re in a tuple because matcher_aux* always expects a list of re's, not a bare one
+                    rep = []
+                    matcher_aux_old(s, (r, ), saved_s_idx, 0, s_len, 1, rep)
+                    found_match = found_match or rep[-1]['success']
+                    reports.append(rep)
+                if found_match:
+                    s_idx = 1 + max(x['end_pos'] for x in reports if x['success'])
+                    re_idx += 1
+                    match_report.append({'expr': cur_regex, 'start_pos': saved_s_idx, 'end_pos': s_idx-1, 'success': True})
+                else:
+                    match_report.append({'expr': cur_regex, 'start_pos': saved_s_idx, 'end_pos': -999, 'success': False})
+                    break
+            case 'star':                
+                print(f"the big star: {cur_regex_expr}")
+                report = []
+                matcher_aux_old(s, cur_regex_expr, s_idx, 0, s_len, len(cur_regex_expr), report)
                 re_idx += 1
             case _:
                 print(f"HOW COULD THIS HAPPEN?? {cur_regex_type}")
                 break
     print("done")
 
+def matcher_aux_v2(s, re, s_idx, re_idx, s_len, re_len, match_report):
+    print("top")
+    # Note: order of tests matters
+    if re_idx >= re_len:
+        print("we are done!")
+        return (True, s_idx, 2000)
+    if s_idx >= s_len:
+        return (False, s_idx, 3000)
+    done = False
+    saved_start_idx = s_idx
+    while not done:
+        print(f"in loop: re_idx: {re_idx}; re: {re}; re_len: {re_len}")        
+        cur_regex = re[re_idx]
+        cur_regex_type = cur_regex[0]
+        cur_regex_expr = cur_regex[1]
+        match cur_regex_type:
+            case 'literal':
+                print(f"a literal: {cur_regex}")
+                cur_char = s[s_idx]
+                rep = {'expr': cur_regex, 'start_pos': s_idx, 'end_pos': s_idx}
+                if cur_char == cur_regex_expr:
+                    s_idx += 1
+                    re_idx += 1
+                    rep['success'] = True
+                    print(f"Literal match!")
+                    match_report.append(rep)
+                else:
+                    rep['success'] = False
+                    print(f"Literal FAIL match")
+                    match_report.append(rep)
+                    # break
+                    return (False, s_idx, s_idx)
+            case 'union':
+                print(f"the union for ever: {cur_regex_expr}")
+                saved_s_idx = s_idx
+                reports = []
+                found_any_match = False
+                max_idx = 0
+                for r in cur_regex_expr:
+                    print(r)                    
+                    # Wrap each sub-re in a tuple because matcher_aux* always expects a list of re's, not a bare one
+                    rep = []
+                    idx = saved_s_idx
+                    found, orig_start_idx, final_idx = matcher_aux(s, (r, ), idx, 0, s_len, 1, rep)
+                    found_any_match = found_any_match or found
+                    max_idx = max([max_idx, idx])
+                if found_any_match:
+                    s_idx = 1 + max_idx
+                    re_idx += 1
+                    match_report.append({'expr': cur_regex, 'start_pos': saved_s_idx, 'end_pos': s_idx-1, 'success': True})
+                else:
+                    match_report.append({'expr': cur_regex, 'start_pos': saved_s_idx, 'end_pos': -999, 'success': False})
+                    # break
+                    return False
+            case 'star':                
+                print(f"the big star: {cur_regex_expr}")
+                report = []
+                saved_idx = s_idx
+                found, orig_start_idx, final_idx = matcher_aux(s, cur_regex_expr, s_idx, 0, s_len, len(cur_regex_expr), report)
+                if found:
+                    s_idx = 1 + final_idx
+                    print(f"Star match: {s[saved_idx:final_idx+1]}")
+                    # do not increment re_idx; try to match again
+                else:
+                    # not matching star is not failure; restore s_idx and keep going
+                    print(f"Star FAIL match: {s[saved_idx:final_idx+1]}")
+                    s_idx = saved_idx
+                    re_idx += 1
+            case _:
+                print(f"HOW COULD THIS HAPPEN?? {cur_regex_type}")
+                break
+        done = s_idx >= s_len or re_idx >= re_len
+    print("done")
+    if re_idx == re_len:
+        return (True, saved_start_idx, s_idx)
+    else:
+        return (False, saved_start_idx, s_idx)
+
+def matcher_aux(s, re, s_idx, re_idx, s_len, re_len, mr):
+    saved_s_idx = s_idx
+    while re_idx < re_len:
+        cur_re = re[re_idx]
+        cur_re_type = cur_re[0]
+        cur_re_expr = cur_re[1]
+        match cur_re_type:
+            case 'literal':
+                if s_idx >= s_len:
+                    return (False, s_idx, s_idx)
+                cur_char = s[s_idx]
+                if cur_char == cur_re_expr:
+                    s_idx += 1
+                    re_idx += 1
+                else:
+                    return (False, s_idx, s_idx)
+            case 'union':
+                print("begin union match")
+                found_any = False
+                max_end_idx = 0
+                for subexpr in cur_re_expr:
+                    found, start, end = matcher_aux(s, subexpr, saved_s_idx, 0, s_len, len(subexpr), mr)
+                    if found:
+                        max_end_idx = max([max_end_idx, end])
+                    found_any = found_any or found
+                if found_any:
+                    s_idx = max_end_idx + 1
+                    re_idx += 1
+                else:
+                    return (False, saved_s_idx, max_end_idx)                
+            case _:
+                print("Whoops NYI")
+                return (False, 999, 999)
+    return (True, saved_s_idx, s_idx - 1)
+                
+    
 def matcher(s, re):
-    matcher_aux(s, re, 0, 0, len(s), len(re))
+    match_report = []
+    retval, start_idx, end_idx = matcher_aux(s, re, 0, 0, len(s), len(re), match_report)
+    print(f"Result: {retval}; {start_idx} - {end_idx}")
 
 print("start")
-# re = "a(d|e)"
-re = (('literal', 'a'), ('union', (('literal', 'd'), ('literal', 'e'))))
-s1 = "ad"
-s2 = "ae"
-
-s_idx = 0
-re_idx = 0
-s_len = len(s)
-re_len = len(re)
-done = False
-match_record = {}
-
+# re1 = "a(d|e)"
+re1 = (('literal', 'a'), ('union', (('literal', 'd'), ('literal', 'e'))))
 # re2 = f(qp)*
 re2 = (('literal', 'f'), ('star', (('literal', 'q'), ('literal', 'p'))))
 # re3 = (ab)*(p|q)(x|y)*z
@@ -138,7 +266,14 @@ re4 = (('star', (('union',
 re5 = (('star', (('literal', 'a'), ('literal', 'b'))), )
 # re6 = (a|b)
 re6 = (('union', (('literal', 'a'), ('literal', 'b'))), )
+re7 = (('literal', 'd'), ('literal', 'f'), ('literal', 'a'), ('literal', 'g'))
+re8 = (('union',
+        (
+            (('literal', 'd'), ('literal', 'f'), ('literal', 'g')),
+            (('literal', 'd'), ('literal', 'f'), ('literal', 'g'), ('literal', 'g'))
+        ) 
+        ), )
+matcher('dfgagg', re8)
 
-matcher('', re3)
-
+# Note cur behaviour is that trailing input is ignored once a match is found
 print("end")
