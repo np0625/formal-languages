f764fcfd450a7bfe5d44474c5b1a0ac637b8965e
Nishad Prakash <nishad@gmail.com>
2026-01-18 22:06:24 -0500

    Change implementation to utilize a count of matched chars. RE11 now working!

--- parse.py
+++ parse.py
@@ -27,6 +27,7 @@ logging.basicConfig(level=logging.DEBUG)
 
 def matcher_aux(s, re, s_idx, re_idx, s_len, re_len, mr):
     saved_s_idx = s_idx
+    chars_matched = 0
     logger.info(f"TOP: re: {re}; s: {s}")
     while re_idx < re_len:
         cur_re = re[re_idx]
@@ -36,43 +37,46 @@ def matcher_aux(s, re, s_idx, re_idx, s_len, re_len, mr):
             case 'literal':
                 logger.info('BEGIN literal match')
                 if s_idx >= s_len:
-                    return (False, s_idx, s_idx)
+                    return (False, s_idx, 0)
                 cur_char = s[s_idx]
                 if cur_char == cur_re_expr:
                     s_idx += 1
+                    chars_matched += 1
                     re_idx += 1
                     logger.debug(f"Literal match success '{cur_char}'")
                 else:
                     logger.debug(f"Literal match fail regex '{cur_re_expr}' vs char '{cur_char}'")
-                    retval = (False, s_idx, s_idx)
+                    retval = (False, s_idx, 0)
                     logger.debug(f"RETURN: {retval}")
                     return retval
             case 'union':
                 logger.info(f"BEGIN union match for {cur_re_expr}")
                 found_any = False
-                max_end_idx = 0
+                max_matched = 0
                 for subexpr in cur_re_expr:
                     logger.debug(f"trying union subexpr {subexpr}")
-                    found, start, end = matcher_aux(s, subexpr, s_idx, 0, s_len, len(subexpr), mr)
+                    found, start, matched = matcher_aux(s, subexpr, s_idx, 0, s_len, len(subexpr), mr)
                     if found:
                         logger.debug(f"found union subexpr {subexpr}")
-                        max_end_idx = max([max_end_idx, end])
+                        max_matched = max([max_matched, matched])
                     found_any = found_any or found
                 if found_any:
                     logger.debug(f"Union match success for {cur_re_expr}")
-                    s_idx = max_end_idx + 1
+                    s_idx += max_matched
+                    chars_matched += max_matched
                     re_idx += 1
                 else:
                     logger.debug(f"Union match fail for {cur_re_expr}")
-                    retval = (False, saved_s_idx, max_end_idx)
+                    retval = (False, saved_s_idx, 0)
                     logger.debug(f"RETURNING: {retval}")
                     return retval
             case 'star':
                 logger.info(f"BEGIN star match: {cur_re_expr}")
-                found, start, end = matcher_aux(s, cur_re_expr, s_idx, 0, s_len, len(cur_re_expr), mr)                
+                found, start, matched = matcher_aux(s, cur_re_expr, s_idx, 0, s_len, len(cur_re_expr), mr)
                 if found:
-                    logger.debug(f"Star match success '{s[s_idx:end+1]}' for expr '{cur_re_expr}' ")
-                    s_idx = end + 1
+                    logger.debug(f"Star match success '{s[s_idx:(s_idx + matched)]}' for expr '{cur_re_expr}' ")
+                    s_idx += matched
+                    chars_matched += matched
                     # try to match the same RE again
                 else:
                     logger.debug(f"Star match failed for {cur_re_expr}")
@@ -84,16 +88,16 @@ def matcher_aux(s, re, s_idx, re_idx, s_len, re_len, mr):
                 retval = (False, 999, 999)
                 logger.debug(f"RETURN: {retval}")
                 return retval
-    retval = (True, saved_s_idx, s_idx - 1 if s_idx > 0 else 0)
+    retval = (True, saved_s_idx, chars_matched)
     logger.info(f"RETURNING FINAL TRUE: {retval}")
     return retval
-                
-    
+
+
 def matcher(re, s):
     match_report = []
-    retval, start_idx, end_idx = matcher_aux(s, re, 0, 0, len(s), len(re), match_report)
-    print(f"Result: {retval}; {start_idx} - {end_idx}: {s[start_idx:end_idx+1]} in {s} for {re}")
-    return (retval, start_idx, end_idx)
+    retval, start_idx, n_matched = matcher_aux(s, re, 0, 0, len(s), len(re), match_report)
+    print(f"Result: {retval}; start: {start_idx} for {n_matched}: {s[start_idx:(start_idx + n_matched)]} in {s} for {re}")
+    return (retval, start_idx, n_matched)
 
 print("start")
 # re1 = "a(d|e)"
@@ -126,7 +130,7 @@ re8 = (('union',
              ('literal', 'g'), ('literal', 'g')),
             (('literal', 'd'), ),
             (('literal', 'a'), ('literal', 'b'))
-        ) 
+        )
         ), )
 re9 = (('literal', 'a'), ('star', (('literal', 'b'), )), ('literal', 'c'))
 # re10 = (ab|cd)k(de)*
@@ -152,21 +156,21 @@ re12 = (('star', (('literal', 'x'), )), ('literal', 'a'))
 re13 = (('star', (('literal', 'x'), ('literal', 'z'))), ('literal', 'a'))
 # matcher(re8, 'dfgaggg')
 # matcher(re9, 'ac')
-# matcher(re10, 'cdkdededededefdf')
+# matcher(re10, 'abkdedededede')
 matcher(re11, 'kde') # WRONG: re11 isn't matching "kde..."!
-# matcher(re12, 'aaaa')
+# matcher(re12, 'xxxxaaaa')
 
-## re14 = x(a|b)(zap)*9
-## re14 = (('literal', 'x'), ('union', ((('literal', 'a'), ), (('literal', 'b'), ))),
-##         ('star', (('literal', 'z'), ('literal', 'a'), ('literal', 'p'))), ('literal', '9'))
-## re14_testers = ('xazap9', 'xbzap9', 'xa9', 'xb9', 'xazapzap9', 'xbzapzapzapzapzap9')
-## re14_results = [t[0] for t in [matcher(re14, x) for x in re14_testers ]]
-## if False in re14_results:
-##     print("PROBLEM!!!")
-## else:
-##     print("All re14 results passed")
-## 
-## print(matcher(re14, 'xazap9'))
+# re14 = x(a|b)(zap)*9
+do_14 = False
+if do_14:
+    re14 = (('literal', 'x'), ('union', ((('literal', 'a'), ), (('literal', 'b'), ))),
+            ('star', (('literal', 'z'), ('literal', 'a'), ('literal', 'p'))), ('literal', '9'))
+    re14_testers = ('xazap9', 'xbzap9', 'xa9', 'xb9', 'xazapzap9', 'xbzapzapzapzapzap9')
+    re14_results = [t[0] for t in [matcher(re14, x) for x in re14_testers ]]
+    if False in re14_results:
+        print("PROBLEM!!!")
+    else:
+        print("All re14 results passed")
 
 # Note cur behaviour is that trailing input is ignored once a match is found
 print("end")
